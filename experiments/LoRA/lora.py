35c2ae034ebba58b5e6aaa4e4f1c0ce989e0b4fd
Minki Jung <qmsoqm2@gmail.com>
2025-01-20 19:15:04 -0500

    Successfully fine-tuned phi-4 with QLoRA

--- experiments/LoRA/lora.py
+++ experiments/LoRA/lora.py
@@ -23,15 +23,14 @@ image = (
 app = modal.App("lm-as-memory-LoRA", image=image)
 vol = modal.Volume.from_name("lm-as-memory", create_if_missing=True)
 
-# modal_name = "HuggingFaceTB/SmolLM2-135M"
-modal_name = "microsoft/phi-4"
+model_name = "microsoft/phi-4"  # 14.7B / 27GB
 
 dataset_path = "HuggingFaceTB/smoltalk"
 dataset_name = "everyday-conversations"
 
 cache_dir = "./.cache/huggingface"
 
-run_name = f"./{modal_name.replace('/', '_')}--{dataset_path.replace('/', '_')}--{datetime.now().strftime("%Y%m%d-%H%M")}"
+run_name = f"./{model_name.replace('/', '_')}--{dataset_path.replace('/', '_')}--{datetime.now().strftime("%Y%m%d-%H%M")}"
 
 
 @app.function(
@@ -48,29 +47,44 @@ def load_tokenizer_and_model():
     os.chdir("/data")
 
     snapshot_download(
-        repo_id=modal_name,
-        # cache_dir=cache_dir,
+        repo_id=model_name,
+        cache_dir=cache_dir,
     )
     vol.commit()
 
     load_dataset(
         dataset_path,
         name=dataset_name,
-        # cache_dir=cache_dir,
+        cache_dir=cache_dir,
     )
     vol.commit()
 
 
 @app.function(
     image=image,
-    gpu="A10g",  # A10g=24GB(1.1$/h) / L4=24GB(0.8$/h) / T4=16GB(0.59$/h)
+    gpu="T4",  # A10g=24GB(1.1$/h) / L4=24GB(0.8$/h) / T4=16GB(0.59$/h)
     volumes={"/data": vol},
 )
 def generate_with_base_model():
     import torch
     import os
     from trl import setup_chat_format
-    from transformers import BitsAndBytesConfig, AutoModelForCausalLM, AutoTokenizer
+    from transformers import (
+        BitsAndBytesConfig,
+        AutoModelForCausalLM,
+        AutoTokenizer,
+        StoppingCriteria,
+        StoppingCriteriaList,
+    )
+
+    class StopOnEndToken(StoppingCriteria):
+        def __init__(self, end_token_id):
+            self.end_token_id = end_token_id
+
+        def __call__(self, input_ids, scores):
+            if input_ids[0][-1] == self.end_token_id:
+                return True
+            return False
 
     os.chdir("/data")
 
@@ -89,20 +103,17 @@ def generate_with_base_model():
     )
 
     model = AutoModelForCausalLM.from_pretrained(
-        modal_name,
-        # cache_dir=cache_dir,
+        model_name,
+        cache_dir=cache_dir,
         quantization_config=config,
         local_files_only=True,
     ).to(device)
 
     tokenizer = AutoTokenizer.from_pretrained(
-        modal_name,
-        # cache_dir=cache_dir,
+        model_name,
+        cache_dir=cache_dir,
         local_files_only=True,
     )
-
-    # model, tokenizer = setup_chat_format(model=model, tokenizer=tokenizer)
-    # print("loaded model and tokenizer")
 
     # Format with template
     messages = [{"role": "user", "content": "Write a haiku about programming"}]
@@ -110,15 +121,20 @@ def generate_with_base_model():
 
     # Generate response
     inputs = tokenizer(formatted_prompt, return_tensors="pt").to(device)
-    outputs = model.generate(**inputs, max_new_tokens=50, repetition_penalty=1.5)
+    outputs = model.generate(
+        **inputs,
+        max_new_tokens=9999,
+        # stopping_criteria=StoppingCriteriaList([StopOnEndToken(end_token_id)]), # Already applied for some reason
+    )
     print("Before training:")
     print(tokenizer.decode(outputs[0], skip_special_tokens=False))
 
 
 @app.function(
     image=image,
-    gpu="A10g",  
+    gpu="A10g",
     volumes={"/data": vol},
+    timeout=1 * 60 * 60,
 )
 def fine_tune_with_lora():
     import os
@@ -145,11 +161,9 @@ def fine_tune_with_lora():
     )
 
     model = AutoModelForCausalLM.from_pretrained(
-        modal_name, cache_dir=cache_dir, quantization_config=config
+        model_name, cache_dir=cache_dir, quantization_config=config
     ).to(device)
-    tokenizer = AutoTokenizer.from_pretrained(modal_name, cache_dir=cache_dir)
-    # model, tokenizer = setup_chat_format(model=model, tokenizer=tokenizer)
-    print("loaded model and tokenizer")
+    tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=cache_dir)
 
     dataset = load_dataset(dataset_path, name=dataset_name, cache_dir=cache_dir)
 
@@ -172,7 +186,7 @@ def fine_tune_with_lora():
     args = SFTConfig(
         output_dir=run_name,  # Directory to save model checkpoints
         num_train_epochs=1,  # Number of training epochs
-        per_device_train_batch_size=2,  # Batch size per GPU
+        per_device_train_batch_size=8,  # Batch size per GPU
         gradient_accumulation_steps=2,  # Accumulate gradients for larger effective batch
         gradient_checkpointing=True,  # Trade compute for memory savings
         optim="adamw_torch_fused",  # Use fused AdamW for efficiency
@@ -185,9 +199,8 @@ def fine_tune_with_lora():
         bf16=True,  # Use bfloat16 precision
         push_to_hub=False,  # Don't push to HuggingFace Hub
         report_to="none",  # Disable external logging
+        max_seq_length=1512,
     )
-
-    max_seq_length = 1512  # max sequence length for model and packing of the dataset
 
     # Create SFTTrainer with LoRA configuration
     trainer = SFTTrainer(
@@ -195,13 +208,7 @@ def fine_tune_with_lora():
         args=args,
         train_dataset=dataset["train"],
         peft_config=peft_config,  # LoRA configuration
-        max_seq_length=max_seq_length,  # Maximum sequence length
         tokenizer=tokenizer,
-        packing=True,  # Enable input packing for efficiency
-        dataset_kwargs={
-            "add_special_tokens": False,  # Special tokens handled by template
-            "append_concat_token": False,  # No additional separator needed
-        },
     )
 
     trainer.train()
@@ -211,32 +218,51 @@ def fine_tune_with_lora():
 @app.function(
     image=image,
     volumes={"/data": vol},
-    gpu="A10g",
+    gpu="T4",
 )
 def generat_with_finetuned_model():
-    from peft import AutoPeftModelForCausalLM
-    from transformers import AutoTokenizer
+    from peft import AutoPeftModelForCausalLM, PeftConfig
+    from transformers import AutoTokenizer, BitsAndBytesConfig
     import torch
+    import os
+
+    os.chdir("/data")
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    model = AutoPeftModelForCausalLM.from_pretrained(run_name).to(device)
-    tokenizer = AutoTokenizer.from_pretrained("facebook/opt-350m")
+    quantization_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_compute_dtype=torch.bfloat16,
+    )
 
+    model = AutoPeftModelForCausalLM.from_pretrained(
+        "./microsoft_phi-4--HuggingFaceTB_smoltalk--20250120-2335",
+        cache_dir=cache_dir,
+        quantization_config=quantization_config,
+        local_files_only=True,
+    ).to(device)
+
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_name,
+        cache_dir=cache_dir,
+        local_files_only=True,
+    )
+
+    messages = [{"role": "user", "content": "Write a haiku about programming"}]
+    formatted_prompt = tokenizer.apply_chat_template(messages, tokenize=False)
+    inputs = tokenizer(formatted_prompt, return_tensors="pt").to(device)
+
+    # Generate response
     model.eval()
-    inputs = tokenizer(
-        "Preheat the oven to 350 degrees and place the cookie dough",
-        return_tensors="pt",
-    )
-
     outputs = model.generate(
-        input_ids=inputs["input_ids"].to("cuda"), max_new_tokens=50
+        **inputs,
+        max_new_tokens=9999,
+        repetition_penalty=1.5,
     )
-    print(
-        tokenizer.batch_decode(
-            outputs.detach().cpu().numpy(), skip_special_tokens=True
-        )[0]
-    )
+    print("After training:")
+    print(tokenizer.decode(outputs[0], skip_special_tokens=False))
 
 
 @app.local_entrypoint()
