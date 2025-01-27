457efe330c5089a963538d0879e2a6b81e977bba
Minki Jung <qmsoqm2@gmail.com>
2025-01-27 17:54:06 -0500

    clean parameters, experiment with 18 rank dimension

--- experiments/LoRA/lora.py
+++ experiments/LoRA/lora.py
@@ -1,6 +1,36 @@
+import time
 import modal
 from datetime import datetime
-from random import uniform
+
+model_name = "microsoft/phi-4"  # 14.7B / 27GB
+dataset_path = "./dataset"  # 27 samples with 4096 tokens
+cache_dir = "./.cache/huggingface"
+run_name = f"{model_name.split('/')[-1]}_LoRA_{datetime.fromtimestamp(time.time() - 5 * 60 * 60).strftime('%m%d-%H:%M')}"
+output_dir = f"./runs/{run_name}"
+
+# LoRA Config
+lora_rank_dimension = 18
+lora_alpha = 8
+lora_dropout = 0.05
+lora_bias = "none"
+lora_target_modules = "all-linear"
+
+# Training Config
+num_train_epochs = 10
+batch_size = 4
+optimizer = "adamw_torch_fused"
+gradient_accumulation_steps = 2
+learning_rate = 2e-4  # Learning rate from QLoRA paper
+max_grad_norm = 0.3  # Gradient clipping threshold for AdamW
+warmup_ratio = 0.03  # Need for AdamW to gather the moving average.
+lr_scheduler_type = "constant"  # Keep learning rate constant after warmup
+max_seq_length = 2048
+gradient_checkpointing = True
+
+# Modal Fine-Tuning Config
+number_of_gpu = 4
+modal_fine_tuning_gpu = f"A10g:{number_of_gpu}"
+
 
 image = (
     modal.Image.debian_slim(python_version="3.12")
@@ -29,49 +59,6 @@ vol = modal.Volume.from_name(
     "lm-as-memory",
     create_if_missing=True,
 )
-
-model_name = "microsoft/phi-4"  # 14.7B / 27GB
-
-dataset_path = "./dataset"
-
-cache_dir = "./.cache/huggingface"
-
-import time
-
-
-def est_time():
-    # Eastern Standard Time is UTC-5 hours
-    return time.time() - 5 * 60 * 60
-
-
-run_name = f"./runs/{model_name.replace('/', '_')}--{dataset_path.replace('/', '').replace('.', '')}--{datetime.fromtimestamp(est_time()).strftime('%Y%m%d-%H%M')}"
-
-default_inputs = [
-    {
-        "role": "LA",
-        "content": "Which newspaper did Etel Adnan work at in Beirut? Hint, Etel Adnan mentioned this in her interview with Laure Adler.",
-    },
-    {
-        "role": "LA",
-        "content": "Which newspaper did Etel Adnan work at in Beirut? Hint, Etel Adnan mentioned this in her interview with Laure Adler.",
-    },
-    {
-        "role": "LA",
-        "content": "Which newspaper did Etel Adnan work at in Beirut? Hint, Etel Adnan mentioned this in her interview with Laure Adler.",
-    },
-    {
-        "role": "LA",
-        "content": "Which newspaper did Etel Adnan work at in Beirut? Hint, Etel Adnan mentioned this in her interview with Laure Adler.",
-    },
-    {
-        "role": "LA",
-        "content": "Which newspaper did Etel Adnan work at in Beirut? Hint, Etel Adnan mentioned this in her interview with Laure Adler.",
-    },
-    {
-        "role": "user",
-        "content": "What condition did Etel Adnan put forward when selling her olive tree paintings?",
-    },
-]  # correct answer = L’Orient-Le Jour.
 
 
 def generate_and_print(
@@ -158,12 +145,14 @@ def load_tokenizer_and_model():
     )
     vol.commit()
 
-    load_dataset(
-        dataset_path,
-        name=dataset_name,
-        cache_dir=cache_dir,
-    )
-    vol.commit()
+    # dataset_path = "HuggingFaceTB/smoltalk"
+    # dataset_name = "everyday-conversations"
+    # load_dataset(
+    #     dataset_path,
+    #     name=dataset_name,
+    #     cache_dir=cache_dir,
+    # )
+    # vol.commit()
 
 
 @app.function(
@@ -174,48 +163,23 @@ def load_tokenizer_and_model():
 )
 def generate_with_base_model():
     import os
-    from random import uniform
     import torch
-    from trl import setup_chat_format
-    from transformers import (
-        BitsAndBytesConfig,
-        AutoModelForCausalLM,
-        AutoTokenizer,
-        StoppingCriteria,
-        StoppingCriteriaList,
-    )
-
-    class StopOnEndToken(StoppingCriteria):
-        def __init__(self, end_token_id):
-            self.end_token_id = end_token_id
-
-        def __call__(self, input_ids, scores):
-            if input_ids[0][-1] == self.end_token_id:
-                return True
-            return False
+    from transformers import BitsAndBytesConfig, AutoModelForCausalLM, AutoTokenizer
 
     os.chdir("/data")
-
-    device = (
-        "cuda"
-        if torch.cuda.is_available()
-        else "mps" if torch.backends.mps.is_available() else "cpu"
-    )
-    print("device:", device)
-
-    config = BitsAndBytesConfig(
-        load_in_4bit=True,
-        bnb_4bit_quant_type="nf4",
-        bnb_4bit_use_double_quant=True,
-        bnb_4bit_compute_dtype=torch.bfloat16,
-    )
 
     model = AutoModelForCausalLM.from_pretrained(
         model_name,
         cache_dir=cache_dir,
-        quantization_config=config,
+        quantization_config=BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_quant_type="nf4",
+            bnb_4bit_use_double_quant=True,
+            bnb_4bit_compute_dtype=torch.bfloat16,
+        ),
         local_files_only=True,
-    ).to(device)
+        device_map="auto",
+    )
 
     tokenizer = AutoTokenizer.from_pretrained(
         model_name,
@@ -223,7 +187,6 @@ def generate_with_base_model():
         local_files_only=True,
     )
 
-    print("Generating with ", model_name)
     generate_and_print(
         model,
         tokenizer,
@@ -245,7 +208,7 @@ def generate_with_base_model():
 
 @app.function(
     image=image,
-    gpu="a10g:2",
+    gpu=modal_fine_tuning_gpu,
     volumes={"/data": vol},
     timeout=1 * 60 * 60,
     secrets=[modal.Secret.from_name("wandb-secret")],
@@ -265,6 +228,8 @@ def fine_tune_with_lora():
     from peft import LoraConfig
     from torch.utils.data import DataLoader
     import wandb
+
+    os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
 
     os.chdir("/data")
 
@@ -297,7 +262,7 @@ def fine_tune_with_lora():
             + [role_A, role_B, "<|im_sep|>"]
         }
     )
-    tokenizer.save_pretrained(run_name + "/tokenizer")
+    tokenizer.save_pretrained(output_dir + "/tokenizer")
 
     print("resize token embeddings with new vocab length: ", len(tokenizer))
     model.resize_token_embeddings(
@@ -308,53 +273,46 @@ def fine_tune_with_lora():
     tokenizer.pad_token = tokenizer.eos_token
 
     dataset = load_from_disk(dataset_path)
-    print("dataset shape:", dataset["input_ids"].shape)
-
-    # Configure LoRA parameters
-    rank_dimension = 6
-    lora_alpha = 8
-    lora_dropout = 0.05
+    print("len_dataset:", len(dataset))
+    print("len_dataset[0]['input_ids']:", len(dataset[0]["input_ids"]))
 
     peft_config = LoraConfig(
-        r=rank_dimension,  # Rank dimension - typically between 4-32
+        r=lora_rank_dimension,  # Rank dimension - typically between 4-32
         lora_alpha=lora_alpha,  # LoRA scaling factor - typically 2x rank
         lora_dropout=lora_dropout,  # Dropout probability for LoRA layers
-        bias="none",  # Bias type for LoRA. the corresponding biases will be updated during training.
-        target_modules="all-linear",  # Which modules to apply LoRA to
+        bias=lora_bias,  # Bias type for LoRA. the corresponding biases will be updated during training.
+        target_modules=lora_target_modules,  # Which modules to apply LoRA to
         task_type="CAUSAL_LM",  # Task type for model architecture
     )
 
-    num_train_epochs = 3
-    batch_size = 1
-    gradient_accumulation_steps = 3
-    learning_rate = 2e-4  # Learning rate (QLoRA paper)
-    max_grad_norm = 0.3  # Gradient clipping threshold for AdamW
-    warmup_ratio = 0.03
-    lr_scheduler_type = "constant"  # Keep learning rate constant after warmup
-    max_seq_length = 4096
-
     wandb.init(
         project="lm-as-memory",
-        name=run_name,
+        name=run_name.split("/")[-1],
         config={
             "model": model_name,
             "dataset": dataset_path,
-            "rank_dimension": rank_dimension,
+            "rank_dimension": lora_rank_dimension,
             "lora_alpha": lora_alpha,
             "lora_dropout": lora_dropout,
-            "learning_rate": learning_rate,
             "num_train_epochs": num_train_epochs,
             "batch_size": batch_size,
+            "gradient_accumulation_steps": gradient_accumulation_steps,
+            "learning_rate": learning_rate,
+            "max_grad_norm": max_grad_norm,
+            "warmup_ratio": warmup_ratio,
+            "lr_scheduler_type": lr_scheduler_type,
+            "max_seq_length": max_seq_length,
+            "extra_note": "Increase rank ",
         },
     )
 
     args = SFTConfig(
-        output_dir=run_name,
+        output_dir=output_dir,
         num_train_epochs=num_train_epochs,
         per_device_train_batch_size=batch_size,
         gradient_accumulation_steps=gradient_accumulation_steps,  # Accumulate gradients for larger effective batch
-        gradient_checkpointing=True,  # Trade compute for memory savings
-        optim="adamw_torch_fused",
+        gradient_checkpointing=gradient_checkpointing,  # Trade compute for memory savings
+        optim=optimizer,
         learning_rate=learning_rate,
         max_grad_norm=max_grad_norm,
         warmup_ratio=warmup_ratio,
@@ -364,6 +322,7 @@ def fine_tune_with_lora():
         bf16=(float_type == torch.bfloat16),
         push_to_hub=False,
         report_to="wandb",
+        save_only_model=True,
         max_seq_length=max_seq_length,
         packing=False,  # Don't concatenate multiple sequences to meet max_seq_length
     )
@@ -389,14 +348,31 @@ def fine_tune_with_lora():
 @app.function(
     image=image,
     volumes={"/data": vol},
-    gpu="A10g",
-    timeout=10 * 60,
+    gpu="T4",
+    timeout=30 * 60,
 )
 def generate_with_finetuned_model():
-    from peft import AutoPeftModelForCausalLM, PeftConfig
+    from peft import AutoPeftModelForCausalLM
     from transformers import AutoTokenizer, BitsAndBytesConfig
     import torch
     import os
+
+    questions = [
+        [
+            {
+                "role": "LA",
+                "content": "Do you think everyone can be an artist?",
+            },  # Everyone is in a certain measure. Yes, everyone wants to express something as well as they can. When I undertake to do something, I do it completely. I do it full-time, like I did with writing. Sometimes one thing, sometimes the other; it’s already a lot and it’s all I did. When I was young, I sometimes helped out with the press, I made cover designs for the books. Sometimes I proofread texts.
+            {"role": "EA", "content": ""},
+        ],
+        [
+            {
+                "role": "LA",
+                "content": "What is “beauty” for you, Etel?",
+            },  # So, it’s very simple. We’re going to start at the beginning. Last October, I got a note from a curator at Tate Modern in London. His name is Achim Borchardt-Hume; he has lots of friends all over the world. I had never met him, but he’d seen something somewhere that I said about Cézanne. And he said to me: “We’re putting on the first ever retrospective of Cézanne in England.” I told him: “I hope you’re going to put The Gardener Vallier in this show.”
+            {"role": "EA", "content": ""},
+        ],
+    ]
 
     os.chdir("/data")
 
@@ -409,28 +385,15 @@ def generate_with_finetuned_model():
         bnb_4bit_compute_dtype=torch.bfloat16,
     )
 
-    lora_model_name = "./runs/microsoft_phi-4--HuggingFaceTB_smoltalk--20250124-1830"
+    lora_model_name = "./runs/phi-4_LoRA_0127-10:44"
 
     tokenizer = AutoTokenizer.from_pretrained(
         lora_model_name + "/tokenizer",
         cache_dir=cache_dir,
         local_files_only=True,
     )
-    print(tokenizer.special_tokens_map)
-    # # tokenizer.chat_template = "{% for message in messages %}{% if (message['role'] == 'system') %}{{'<|im_start|>system<|im_sep|>' + message['content'] + '<|im_end|>'}}{% elif (message['role'] == 'LA') %}{{'<|im_start|>&29njkn(dkj38$%nkjn#<|im_sep|>' + message['content'] + '<|im_end|>'}}{% elif (message['role'] == 'EA') %}{{'<|im_start|>foi%ioh!@oih(&idl*<|im_sep|>' + message['content'] + '<|im_end|>'}}{% elif (message['role'] == 'user') %}{{'<|im_start|>user<|im_sep|>' + message['content'] + '<|im_end|>'}}{% elif (message['role'] == 'assistant') %}{{'<|im_start|>assistant<|im_sep|>' + message['content'] + '<|im_end|>'}}{% endif %}{% endfor %}"
-    # tokenizer.chat_template = "{% for message in messages %}{% if (message['role'] == 'system') %}{{'<|im_start|>system<|im_sep|>' + message['content'] + '<|im_end|>'}}{% elif (message['role'] == 'LA') %}{{'<|im_start|>&29njkn(dkj38$%nkjn#<|im_sep|>' + message['content'] + '<|im_end|>'}}{% elif (message['role'] == 'EA') %}{{'<|im_start|>foi%ioh!@oih(&idl*<|im_sep|>' + message['content'] + '<|im_end|>'}}{% endif %}{% endfor %}"
 
-    # role_A = "#29njkn(dkj38$%nkjn#"  # Laure Adler
-    # role_B = "#foi*Ewoh!@oih(&idl#"  # Etel Adnan
-    # tokenizer.add_special_tokens(
-    #     {
-    #         "additional_special_tokens": tokenizer.additional_special_tokens
-    #         + [role_A, role_B, "<|im_sep|>"]
-    #     }
-    # )
-
-    checkpoint_steps = [32, 48, 64]
-    # checkpoint_steps = [16, 32, 48, 64, 80, 96, 112, 128, 144, 160]
+    checkpoint_steps = [30]
 
     for step in checkpoint_steps:
         print(f">>>>>>> step {step} >>>>>>>")
@@ -439,28 +402,14 @@ def generate_with_finetuned_model():
             cache_dir=cache_dir,
             quantization_config=quantization_config,
             local_files_only=True,
-        ).to(device)
-
-        q1 = [
-            {
-                "role": "LA",
-                "content": "What is “beauty” for you, Etel?",
-            },  # So, it’s very simple. We’re going to start at the beginning. Last October, I got a note from a curator at Tate Modern in London. His name is Achim Borchardt-Hume; he has lots of friends all over the world. I had never met him, but he’d seen something somewhere that I said about Cézanne. And he said to me: “We’re putting on the first ever retrospective of Cézanne in England.” I told him: “I hope you’re going to put The Gardener Vallier in this show.”
-            {"role": "EA", "content": ""},
-        ]
-        q2 = [
-            {
-                "role": "LA",
-                "content": "Do you think everyone can be an artist?",
-            },  # Everyone is in a certain measure. Yes, everyone wants to express something as well as they can. When I undertake to do something, I do it completely. I do it full-time, like I did with writing. Sometimes one thing, sometimes the other; it’s already a lot and it’s all I did. When I was young, I sometimes helped out with the press, I made cover designs for the books. Sometimes I proofread texts.
-            {"role": "EA", "content": ""},
-        ]
+            device_map="auto",
+        )
 
         generate_and_print(
             model,
             tokenizer,
             device,
-            inputs=[q1, q2],
+            inputs=questions,
             max_new_tokens=50,
             generation_prompt=False,
             skip_special_tokens=False,
@@ -479,6 +428,6 @@ def main():
 
     # generate_with_base_model.remote()
 
-    fine_tune_with_lora.remote()
+    # fine_tune_with_lora.remote()
 
-    # generate_with_finetuned_model.remote()
+    generate_with_finetuned_model.remote()
